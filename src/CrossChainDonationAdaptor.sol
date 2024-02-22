ba9a13dc0f7e37958a16b9379d89ef11cb241bfa
MrDeadCe11 <mister.deadce11@gmail.com>
2024-02-22 08:10:11 -0600

    changed adaptor to call allocate on allo core.

--- src/CrossChainDonationAdaptor.sol
+++ src/CrossChainDonationAdaptor.sol
@@ -4,9 +4,10 @@ pragma solidity 0.8.20;
 import {ForwarderXReceiver} from "connext-integration/contracts/destination/xreceivers/ForwarderXReceiver.sol";
 import {IStrategy} from "./interfaces/IStrategy.sol";
 contract CrossChainDonationAdaptor is ForwarderXReceiver {
-
-    constructor(address _connext) ForwarderXReceiver(_connext) {}
-
+    address public alloV2;
+    constructor(address _connext, address _allo) ForwarderXReceiver(_connext) {
+        alloV2 = _allo;
+    }
     function _forwardFunctionCall(
         bytes memory _preparedData,
         bytes32, /*_transferId*/
@@ -21,10 +22,10 @@ contract CrossChainDonationAdaptor is ForwarderXReceiver {
         (bytes memory _forwardCallData, , , ) =
             abi.decode(_preparedData, (bytes, bytes32, uint256, address));
 
-        (bytes memory _alloData, address _alloStrategy, address _sender) =
-         abi.decode(_forwardCallData, (bytes, address, address));
+        (bytes memory _alloData, uint256 _poolId) =
+         abi.decode(_forwardCallData, (bytes, uint256));
         
-        IStrategy(_alloStrategy).allocate(_alloData, _sender);
+        IAllo(alloV2).allocate(_poolId, _alloData);
 
         return true;
     }
