8c9847fd9510cc86d4428c391d57502730f0ba3c
bitbeckers <code@bitbeckers.com>
2024-03-05 14:04:08 +0100

    chore(wrapup): deploy other chains and write readme

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -13,9 +13,12 @@ contract Deploy is BaseScript {
     address public connextCore;
 
     function run() public broadcast returns (MockAllo allo, CrossChainDonationAdapter adapter) {
-        if (block.chainid != 10) revert UnsupportedChainId();
+        if (block.chainid != 10 && block.chainid != 137 && block.chainid != 42_161 && block.chainid != 250) revert UnsupportedChainId();
 
         if (block.chainid == 10) connextCore = 0x8f7492DE823025b4CfaAB1D34c58963F2af5DEDA;
+        if (block.chainid == 137) connextCore = 0x11984dc4465481512eb5b777E44061C158CF2259;
+        if (block.chainid == 42_161) connextCore = 0xEE9deC2712cCE65174B561151701Bf54b99C24C8;
+        
 
         allo = new MockAllo();
         adapter = new CrossChainDonationAdapter(connextCore, address(allo));
